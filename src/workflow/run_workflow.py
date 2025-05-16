9bb818a49c3a35d907c92361616df930a0e7df67
Muhammad Ahmad <muhammad.ahmad18802@gmail.com>
2025-05-16 19:41:28 +0500

    fixed logged workflow dependcies issues - added a workflow outside src

---
+++ src/workflow/run_workflow.py
@@ -0,0 +1,21 @@
+from prefect import flow, task, get_run_logger
+from .ingest_and_store import ProcessingWorkflow  
+
+@task
+def run_processing_task(query: str = "cs.AI", num_papers: int = 5, max_extensions: int = 1):
+    logger = get_run_logger()
+    logger.info("Starting ProcessingWorkflow through Prefect...")
+
+    # Initialize with logger
+    workflow = ProcessingWorkflow(output_file="parsed_papers.jsonl", criterion="relevance", logger=logger)
+
+    workflow.run_single(query=query, num_papers=num_papers, max_extensions=max_extensions)
+
+    logger.info("ProcessingWorkflow completed with Prefect...")
+
+@flow(name="Preprocessing: Storing Workflow with Prefect Logging")
+def processing_workflow_with_logging():
+    run_processing_task()       #pass arguments here
+
+if __name__ == "__main__":
+    processing_workflow_with_logging()
