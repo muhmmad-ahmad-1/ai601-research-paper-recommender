b3706812afdf16eb0ea9bb3a7c3cd798d31b01a1
Muhammad Ahmad <muhammad.ahmad18802@gmail.com>
2025-05-16 22:26:34 +0500

    fix qroq

--- src/ingestion/openrouter_api.py
+++ src/ingestion/openrouter_api.py
@@ -3,8 +3,7 @@ from groq import Groq
 import time
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-def query_openrouter(prompt: str, api_key: str, max_length: int = 500, model: str = "llama3-8b-8192") -> str:
-    print(api_key)
+def query_openrouter(prompt: str, api_key: str, logger = None, max_length: int = 500, model: str = "llama3-8b-8192") -> str:
     client = Groq(api_key=api_key)
 
     max_retries = 5
@@ -17,12 +16,12 @@ def query_openrouter(prompt: str, api_key: str, max_length: int = 500, model: st
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0
             )
-            print("Completion choice Message", completion.choices[0].message.content)
-            print("Completion choice Finish Reason", completion.choices[0].finish_reason)
+            logger.info("Completion choice Message", completion.choices[0].message.content)
+            logger.info("Completion choice Finish Reason", completion.choices[0].finish_reason)
             return completion.choices[0].message.content
         except Exception as e:
-            print(f"Attempt {attempt + 1} failed: {str(e)}")
+            logger.error(f"Attempt {attempt + 1} failed: {str(e)}")
             time.sleep(delay)
     
-    print("All retry attempts failed.")
+    logger.error("All retry attempts failed.")
     return ""
